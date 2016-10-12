352f40b7f59d989f512566b56da080570f317c3d
Val Catalasan <vcatalasan@gmail.com>
2016-10-12 15:58:10 -0700

    implemented root method

--- src/warmup/Quadratic.java
+++ src/warmup/Quadratic.java
@@ -1,5 +1,8 @@
 package warmup;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Quadratic {
@@ -12,7 +15,22 @@ public class Quadratic {
      * @return all integers x such that ax^2 + bx + c = 0.
      */
     public static Set<Integer> roots(int a, int b, int c) {
-        throw new RuntimeException("not implemented yet;"); // TODO: delete this line when you implement it
+        //throw new RuntimeException("not implemented yet;"); // TODO: delete this line when you implement it
+        //Finding out the roots
+        Set<Integer> root = new HashSet<>();
+
+        int discriminant = (b * b) - (4 * a * c);
+
+        if (discriminant >= 0 || Math.abs(c) < 2_000_000_000) {
+            int sqrt = (int) Math.sqrt(discriminant);
+            root.add((-b + sqrt) / (2 * a));
+            root.add((-b - sqrt) / (2 * a));
+        } else {
+            int sqrt = (int) Math.sqrt(Math.abs(c));
+            root.add(sqrt);
+            root.add(-sqrt);
+        }
+        return root;
     }
 
     
